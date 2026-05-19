f7604f35da1b37750ab105f0da97add26062a1d0
KetchOxy <manuelsechi330@gmail.com>
2026-05-19 09:33:36 +0200

    Refactor damage dice input handling in Tirocolpire

--- com/generation/italy/dnd/colpire/Tirocolpire.java
+++ com/generation/italy/dnd/colpire/Tirocolpire.java
@@ -1,19 +1,19 @@
 package com.generation.italy.dnd.colpire;
-
+ 
 import com.generation.italy.dnd.simdado.Dado;
 import com.generation.italy.dnd.abilita.Tiroabilita;
 import com.generation.library.*;
-
+ 
 // TIRO COLPIRE - gestisce il tiro per colpire, CA e dadi danno
 public class Tirocolpire {
-
+ 
     public static void esegui(int modificatore) {
-
+ 
         System.out.println("--- Tiro per colpire ---");
-
-        // LANCIO D20 - riutilizzo il metodo di TiroAbilita
+ 
+        // LANCIO D20 - riutilizzo il metodo di Tiroabilita
         int tiroBase = Tiroabilita.lanciaD20();
-
+ 
         if (tiroBase == 1) {
             // FUMBLE - mancato automatico, modificatore non applicato
             System.out.println("*** FUMBLE! Hai mancato! ***");
@@ -26,60 +26,52 @@ public class Tirocolpire {
                 // CRITICO - colpisce sempre indipendentemente dalla CA
                 System.out.println("*** CRITICO! ***");
             }
-
+ 
             // APPLICO IL MODIFICATORE
             int tiroFinale = tiroBase + modificatore;
             System.out.println("Tiro finale: " + tiroBase + " + mod(" + modificatore + ") = " + tiroFinale);
-
+ 
             // CONFRONTO CON LA CLASSE ARMATURA
             System.out.print("Classe Armatura del nemico? ");
             int ca = Console.readInt();
-
+ 
             if (tiroFinale < ca && tiroBase != 20) {
                 System.out.println("Hai mancato! CA nemica: " + ca);
             } else {
-
-                // COLPO ANDATO A SEGNO - chiedo i dadi danno all'utente
+ 
+                // COLPO ANDATO A SEGNO
                 System.out.println("Hai colpito! Tiro il danno...");
                 int totale = 0;
                 String riepilogo = "";
-
+ 
                 // CICLO DADI DANNO
                 boolean AltroDado = true;
                 while (AltroDado) {
-
-                    // L'UTENTE INSERISCE LE FACCE
-                    System.out.print("Quante facce ha il dado danno? ");
-                    int facce = Console.readInt();
-
-                    // VALIDAZIONE DADO tramite Dado.dadoValido()
-                    while (Dado.dadoValido(facce) == false) {
-                        System.out.println("Dado non valido! Scegli tra: 4, 6, 8, 10, 12, 20, 100");
-                        System.out.print("Quante facce ha il dado? ");
-                        facce = Console.readInt();
-                    }
-
+ 
+                    // DADO.CHIEDIGFACCE() gestisce input e validazione
+                    int facce = Dado.chiedigFacce();
+ 
                     System.out.print("Quanti dadi lanci? ");
                     int q = Console.readInt();
-
+ 
                     // FOR - chiama Dado.tira() per ogni dado
                     for (int i = 0; i < q; i++) {
                         int risultato = Dado.tira(facce);
                         totale += risultato;
                         riepilogo += "D" + facce + ": " + risultato + "  ";
                     }
-
+ 
                     System.out.print("Hai altri dadi? (s/n): ");
                     String risposta = Console.readString();
                     AltroDado = risposta.equals("s");
                 }
-
+ 
                 // SE CRITICO - il danno viene raddoppiato
                 if (tiroBase == 20) {
                     totale = totale * 2;
                     System.out.println("*** Danno raddoppiato: " + totale + " ***");
                 }
-
+ 
                 // RIEPILOGO FINALE
                 System.out.println("--- Riepilogo dadi ---");
                 System.out.println(riepilogo);
